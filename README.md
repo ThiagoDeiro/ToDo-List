e2a343c15f9afd79350a261ebbe89595bacf932d
gabriel ponciano <bielponciano@hotmail.com>
2022-01-20 12:36:56 +1100

    updated

---
+++ README.md
@@ -0,0 +1,29 @@
+# To do list
+
+## Description:
+
+This to do list app was designed for users who have the need to organise their daily, weekly or monthly tasks. As developers, we tend to receive many tasks throughout a project. Therefore, this has motivated us to create an app where we can organise and prioritise tasks. 
+
+It is relevant to highlight that the local storage will not clear your data after refreshing the page. The intention is to keep your tasks saved until you have finalised it. 
+
+
+
+## Instructions:
+For those who are new users of the app, find some instructions below:
+
+*In order to add a new item to your list, type in the input field and click in the plus sign on the right.
+*Once your item is added to the list, you have 3 options: mark the task as done, edit it or delete it. 
+*At the bottom of list, there are 2 additional buttons: clear all and history.
+*Clear all button: once you are finalised, this button will clear all your tasks, without the need of selecting them individually. 
+*History button: this is where the users can find tasks that were cleared from the list. 
+*Once all your tasks are done, a "you are up to date message" should be displayed. 
+
+## Screenshots:
+
+
+
+# Technology stack:
+*HTML
+*CSS 
+*Java Script
+
--- script.js
+++ script.js
@@ -4,133 +4,150 @@ let ul = document.getElementById("toDoListParent");
 var toDos = JSON.parse(localStorage.getItem("Content")) || [];
 
 function createId() {
-  return Math.random().toString(36).substring(2, 9);
+    return Math.random().toString(36).substring(2, 9);
 }
 
 function renderData() {
-  ul.innerHTML = "";
-  let taskContent;
-  let ind;
+    ul.innerHTML = "";
+    let taskContent;
+    let ind;
 
-  if (toDos.length === 0) {
-    let h2 = document.createElement("h2");
-    h2.innerHTML = "You're up-to-date";
-    ul.appendChild(h2);
-  } else {
-    // ToDo.lenght= 10
-    for (let i = 0; i < toDos.length; i++) {
-      taskContent = toDos[i].Content;
-      ind = [i];
+    if (toDos.length === 0) {
+        let h2 = document.createElement("h2");
+        h2.innerHTML = "You're up-to-date";
+        ul.appendChild(h2);
+    } else {
+        // ToDo.lenght= 10
+        for (let i = 0; i < toDos.length; i++) {
+            taskContent = toDos[i].Content;
+            ind = [i];
 
-      let li = document.createElement("li");
-      li.setAttribute("id", "todoElement");
-      li.innerHTML = taskContent;
+            let li = document.createElement("li");
+            li.setAttribute("id", "todoElement");
+            li.innerHTML = taskContent;
 
-      let checkbox = document.createElement("input");
-      checkbox.setAttribute("type", "checkbox");
-      checkbox.setAttribute("id", "checkbox");
-      checkbox.addEventListener("change", function () {
-        if (this.checked) {
-          li.style.textDecoration = "line-through";
-        } else {
-          li.style.textDecoration = "none";
+            let checkbox = document.createElement("input");
+            checkbox.setAttribute("type", "checkbox");
+            checkbox.setAttribute("id", "checkbox");
+            checkbox.addEventListener("change", function() {
+                if (this.checked) {
+                    li.style.textDecoration = "line-through";
+                } else {
+                    li.style.textDecoration = "none";
+                }
+            });
+
+            let deleteButton = document.createElement("a");
+            deleteButton.setAttribute("onclick", `deletToDo(${ind})`);
+            deleteButton.setAttribute("id", `deleteButton`);
+            deleteButton.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+
+            let editButton = document.createElement("a");
+            editButton.setAttribute("onclick", `editToDo(${ind})`);
+            editButton.setAttribute("id", `editButton`);
+            editButton.innerHTML = `<i class="fas fa-edit"></i>`;
+
+            /*let clearButton = document.createElement("a");
+            clearButton.setAttribute("onclick", `clearAll(${ind})`);
+            clearButton.setAttribute("id", `clearButton`);
+            clearButton.innerHTML = "Clear All";*/
+
+            let span = document.createElement("span");
+            span.setAttribute("id", "spanContainer");
+
+            let spanFor = document.createElement("span");
+            spanFor.setAttribute("id", "spanMainEl");
+
+            let spanForButton = document.createElement("span");
+            spanForButton.setAttribute("id", "spanForButton");
+
+            spanFor.appendChild(checkbox);
+            spanFor.appendChild(li);
+
+            spanForButton.appendChild(editButton);
+            spanForButton.appendChild(deleteButton);
+            span.appendChild(spanFor);
+            span.appendChild(spanForButton);
+            ul.appendChild(span);
         }
-      });
-
-      let deleteButton = document.createElement("a");
-      deleteButton.setAttribute("onclick", `deletToDo(${ind})`);
-      deleteButton.setAttribute("id", `deleteButton`);
-      deleteButton.innerHTML = `<i class="fas fa-trash-alt"></i>`;
-
-      let editButton = document.createElement("a");
-      editButton.setAttribute("onclick", `editToDo(${ind})`);
-      editButton.setAttribute("id", `editButton`);
-      editButton.innerHTML = `<i class="fas fa-edit"></i>`;
-
-      let span = document.createElement("span");
-      span.setAttribute("id", "spanContainer");
-
-      let spanFor = document.createElement("span");
-      spanFor.setAttribute("id", "spanMainEl");
-
-      let spanForButton = document.createElement("span");
-      spanForButton.setAttribute("id", "spanForButton");
-
-      spanFor.appendChild(checkbox);
-      spanFor.appendChild(li);
-
-      spanForButton.appendChild(editButton);
-      spanForButton.appendChild(deleteButton);
-      span.appendChild(spanFor);
-      span.appendChild(spanForButton);
-      ul.appendChild(span);
     }
-  }
 }
 
 function addNewToDo() {
-  let inputValue = inputField.value;
+    let inputValue = inputField.value;
 
-  let dataFormat = {
-    Content: inputValue,
-    id: createId(),
-  };
+    let dataFormat = {
+        Content: inputValue,
+        id: createId(),
+    };
 
-  if (inputValue !== "") {
-    toDos.push(dataFormat);
-    inputField.value = "";
+    if (inputValue !== "") {
+        toDos.push(dataFormat);
+        inputField.value = "";
 
-    renderData();
-    saveToStorage();
-  } else {
-    alert("Input must be filled");
-  }
+        renderData();
+        saveToStorage();
+    } else {
+        alert("Input must be filled");
+    }
+    console.log(localStorage);
 }
 
 function deletToDo(ind) {
-  toDos.splice(ind, 1);
-  renderData();
-  saveToStorage();
+    toDos.splice(ind, 1);
+    renderData();
+    saveToStorage();
 }
 
 function editToDo(ind) {
-  // * todos[2].id
-  let toDoText = toDos[ind].Content; // toDoText = fazer compras;
-  inputField.value = toDoText; //Input field => toDotext
+    // * todos[2].id
+    let toDoText = toDos[ind].Content; // toDoText = fazer compras;
+    inputField.value = toDoText; //Input field => toDotext
 
-  let mainButton = document.getElementById("addNewToDo");
-  mainButton.style.display = "none";
+    let mainButton = document.getElementById("addNewToDo");
+    mainButton.style.display = "none";
 
-  let saveButton = document.getElementById("saveNewTask");
-  saveButton.style.display = "inline-block";
+    let saveButton = document.getElementById("saveNewTask");
+    saveButton.style.display = "inline-block";
 
-  saveButton.setAttribute("onclick", `replaceToDo(${ind})`);
+    saveButton.setAttribute("onclick", `replaceToDo(${ind})`);
 }
 
 function replaceToDo(ind) {
-  // console.log(ind);
-  let todoText = toDos[ind].Content;
-  todoText = inputField.value;
-  if (ind !== -1) {
-    toDos[ind].Content = todoText;
+    // console.log(ind);
+    let todoText = toDos[ind].Content;
+    todoText = inputField.value;
+    if (ind !== -1) {
+        toDos[ind].Content = todoText;
 
-    renderData();
-    saveToStorage();
+        renderData();
+        saveToStorage();
 
-    let mainButton = document.getElementById("addNewToDo");
-    mainButton.style.display = "inline-block";
+        let mainButton = document.getElementById("addNewToDo");
+        mainButton.style.display = "inline-block";
 
-    let saveButton = document.getElementById("saveNewTask");
-    saveButton.style.display = "none";
+        let saveButton = document.getElementById("saveNewTask");
+        saveButton.style.display = "none";
 
-    inputField.value = "";
-  }
+        inputField.value = "";
+    }
 }
 
+function clearAll() {
+    toDos.length = 0;
+    saveToStorage();
+    renderData();
+    if (toDos.length === 0) {
+        console.log(localStorage);
+    }
+
+}
+
+
 function saveToStorage() {
-  localStorage.setItem("Content", JSON.stringify(toDos));
+    localStorage.setItem("Content", JSON.stringify(toDos));
 }
 console.log(toDos);
 renderData();
 
-// window.localStorage.clear();
+//window.localStorage.clear();
